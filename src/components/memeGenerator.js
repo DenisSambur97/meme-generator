10d879bb5c7629ba13a04a9c18b2ec16327086f6
Denis <denissand1997@gmail.com>
2023-02-19 19:29:00 +0300

    app

---
+++ src/components/memeGenerator.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+
+class MemeGenerator extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return(
+            <h1>Meme Generator</h1>
+        )
+    }
+}
+
+export default MemeGenerator
